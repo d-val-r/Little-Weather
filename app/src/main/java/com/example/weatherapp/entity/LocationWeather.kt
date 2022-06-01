d75259287ea897b33d2c79b4fd4d1f3e47f01514
d-val-r <dvrudenya@noctrl.edu>
2022-06-01 17:28:51 -0500

    fix nested JSON access error

--- app/src/main/java/com/example/weatherapp/entity/LocationWeather.kt
+++ app/src/main/java/com/example/weatherapp/entity/LocationWeather.kt
@@ -1,9 +1,14 @@
 package com.example.weatherapp.entity
-
 import com.squareup.moshi.Json
 
+// the main JSON response consists of nested JSON objects
+// use this secondary class to load the JSON strings into objects
+// with Moshi
+data class ListItems(val dt: Int, val main: Map<String, Float>)
+
+// the main JSON response
 data class LocationWeather(
-    @Json(name = "list") val conditions: List<Any>
+    @Json(name = "list") val conditions: List<ListItems>,
 ) {
-    val temp = conditions[0]
+
 }
--- app/src/main/java/com/example/weatherapp/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/weatherapp/overview/OverviewViewModel.kt
@@ -24,7 +24,7 @@ class OverviewViewModel : ViewModel() {
         WeatherApi.retrofitService.getWeatherFromAPI().enqueue(
             object: Callback<LocationWeather> {
                 override fun onResponse(call: Call<LocationWeather>, response: Response<LocationWeather>) {
-                    _response.value = response.body()?.temp.toString()
+                    _response.value = response.body()?.conditions?.get(0)?.main?.get("temp").toString()
                 }
 
                 override fun onFailure(call: Call<LocationWeather>, t: Throwable) {
