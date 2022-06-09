4e795d251447a35415ba26c0b32c0167690f0a52
d-val-r <dvrudenya@noctrl.edu>
2022-06-09 12:11:59 -0500

    add state and main weather display to home page

--- app/src/main/java/com/example/weatherapp/entity/Entry.kt
+++ app/src/main/java/com/example/weatherapp/entity/Entry.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.entity
 
+import android.view.View
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -32,4 +33,8 @@ data class Entry(
     // converts the temperature to a Fahrenheit representation and stores it as a
     // string to render in a TextView
     var fTemp = ((temperature - 273.15) * (9.0/5.0) + 32).toString()
+
+    // if the country name is the US set
+    // the visibility variable to later be referenced by the TextView
+    var stateVisibility = if (countryName == "US") View.VISIBLE else View.GONE
 }
--- app/src/main/java/com/example/weatherapp/responses/Location.kt
+++ app/src/main/java/com/example/weatherapp/responses/Location.kt
@@ -1,4 +1,4 @@
 package com.example.weatherapp.responses
 
 // the main JSON response
-data class Location(val name: String, val country: String, val state: String, val lat: Float, val lon: Float)
+data class Location(val name: String, val country: String, val state: String,  val lat: Float, val lon: Float)
